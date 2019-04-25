d8887492415225f6afe559f64d78d4d62ebb2a93
Suhail Abood <suhail.abood@gmail.com>
2019-04-25 19:38:13 +0100

    add environment variable consts

---
+++ proton/const.go
@@ -0,0 +1,16 @@
+package proton
+
+const (
+	// EnvMySQLHost environment variable to use to access SQL host service
+	EnvMySQLHost = "MYSQL_HOST"
+	// EnvMySQLPort environment variable to use to access SQL port
+	EnvMySQLPort = "MYSQL_PORT"
+	// EnvMySQLUser environment variable to use for SQL user
+	EnvMySQLUser = "MYSQL_USER"
+	// EnvMySQLPassword environment variable to use for SQL password
+	EnvMySQLPassword = "MYSQL_PWD"
+	// EnvThirdPartyAPIKey environment variable to use for API key
+	EnvThirdPartyAPIKey = "THIRD_PARTY_API_KEY"
+	// EnvThirdPartyAPISecret environment variable to use for API secret
+	EnvThirdPartyAPISecret = "THIRD_PARTY_API_SECRET"
+)
